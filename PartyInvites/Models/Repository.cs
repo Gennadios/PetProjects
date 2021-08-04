8ff6030f64ab8dc4eba6db516865a7678ce55e73
Gennadios <egon.sch@yandex.ru>
2021-08-04 21:03:56 +0300

    added aspnetcore project with basic routing and form

---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace PartyInvites.Models
+{
+    public static class Repository
+    {
+        private static List<GuestResponse> _responses = new List<GuestResponse>();
+
+        public static IEnumerable<GuestResponse> Responses => _responses;
+
+        public static void AddResponse(GuestResponse response) => _responses.Add(response);
+    }
+}
