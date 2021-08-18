b5bc1a97bd1fb85d2398ec8d2bc24ff41cafb096
Gennadios <egon.sch@yandex.ru>
2021-08-18 15:38:30 +0300

    removed typo from appsetings, added test method for page breaks

--- SportsSln/SportsStore/Controllers/HomeController.cs
+++ SportsSln/SportsStore/Controllers/HomeController.cs
@@ -1,14 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using System.Linq;
 
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
         private IStoreRepository _repository;
+        public int PageSize = 4;
 
         public HomeController(IStoreRepository repository) => _repository = repository;
 
-        public IActionResult Index() => View(_repository.Products);
+        public IActionResult Index(int productPage = 1) =>
+            View(_repository.Products
+            .OrderBy(p => p.ProductID)
+            .Skip((productPage - 1) * PageSize)
+            .Take(PageSize));
     }
 }
