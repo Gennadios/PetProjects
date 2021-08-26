375a2b95dab43829566adcb0538546637554277a
Gennadios <egon.sch@yandex.ru>
2021-08-26 20:17:56 +0300

    added navigation bar and filter by category

---
+++ SportsSln/SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IStoreRepository _repository;
+
+        public NavigationMenuViewComponent(IStoreRepository repo)
+        {
+            _repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+            return View(_repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
